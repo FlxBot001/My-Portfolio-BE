305d3d0c91875cc4b193b8f1945e2e046451e0db
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-19 02:02:14 +0300

    Updated add blog pag to include blog component

--- pages/blogs/addblog.js
+++ pages/blogs/addblog.js
@@ -1,3 +1,5 @@
+import Blog from "@/components/Blog";
+import { FcSurvey } from "react-icons/fc";
 
 
 export default function Addblog() {
@@ -9,7 +11,14 @@ export default function Addblog() {
             <div className="titledashboard flex flex-sb">
                 <div>
                     <h2>Add <span>Blog</span></h2>
+                    <h3>ADMIN PANEL</h3>
                 </div>
+                <div className="breadcrumb">
+                    <FcSurvey /> <span>/</span> <span>Addblog</span>
+                </div>
+            </div>
+            <div className="blogsadd">
+                <Blog />
             </div>
         </div>
     </>
