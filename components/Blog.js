b2f4bb024d9e625ddb045bdcc91e75699e759331
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-19 02:47:25 +0300

    Updates

--- components/Blog.js
+++ components/Blog.js
@@ -8,7 +8,19 @@ export default function Blog() {
 
 
     return <>
+        <form className="addWebsiteform">
+            {/* blog title */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+                <label htmlFor="title">Title</label>
+                <input type="text" id='title' placeholder='Enter title' />
+            </div>
 
+            {/* blog slug url */}
+            <div className="w-100 flex flex-col flex-left mb-2">
+                <label htmlFor="title">Slug (SEO friendly URL)</label>
+                <input type="text" id='slug' placeholder='Enter Slug URL' />
+            </div>
+        </form>
     </>
 }
 
