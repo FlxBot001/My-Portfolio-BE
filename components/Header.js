90e4fb8b88677351e28682316ef7d9a89c1173f5
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-19 01:13:44 +0300

    Cteated the navbar with almost all components

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,43 @@
+import { useState } from "react";
+import { FcLeftDown, FcMenu, FcRightUp } from "react-icons/fc";
 
 
 export default function Header() {
-   
+
+    const [isFullscreen, setIsFullscreen] = useState(false);
+
+    const toggleFullScreen = () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen().then(() => {
+                setIsFullscreen(true);
+            })
+        } else {
+            document.exitFullscreen().then(() => {
+                setIsFullscreen(false);
+            })
+        }
+    }
+
     return <>
         <header className="header flex flex-sb">
             <div className="logo flex gap-2">
                 <h1>ADMIN</h1>
                 <div className="headerham flex flex-center">
-                    
+                    <FcMenu />
+                </div>
+            </div>
+            <div className="rightnav flex gap-2">
+                <div onClick={toggleFullScreen}>
+                    {isFullscreen ? <FcLeftDown /> : <FcRightUp />}
+                </div>
+                <div className="notification">
+                    <img src="/img/notification.png" alt="notification" />
+                </div>
+                <div className="profilenav">
+                    <img src="/img/user.png" alt="user" />
                 </div>
             </div>
         </header>
-   
+
     </>
 }
