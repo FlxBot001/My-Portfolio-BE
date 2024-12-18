e8c379732e1b80db277e54c9054588693d1632f8
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-19 01:51:51 +0300

    Updated Add blog page title and container on container props to move with aside

--- pages/blogs/addblog.js
+++ pages/blogs/addblog.js
@@ -5,6 +5,12 @@ export default function Addblog() {
 
 
     return <>
-       
+        <div className="addblogpage">
+            <div className="titledashboard flex flex-sb">
+                <div>
+                    <h2>Add <span>Blog</span></h2>
+                </div>
+            </div>
+        </div>
     </>
 }
