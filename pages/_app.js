5fd3f572e1f09a817b7458ab0d82d62697172991
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-21 16:43:39 +0300

    Updated app.js file to mplement loading function, for the components

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,62 @@
-
 import "@/styles/globals.css";
 import ParentComponent from "@/components/ParentComponent";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Loading from "@/components/Loading";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter(); // use userouter hook
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+
+    // check if the route is already complete when the component mounts
+    if (router.isReady) {
+      setLoading(false);
+    }
+
+    // add event listeners on component mount
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    // remove event listeners on component unmount
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+
+
+  },[router.isReady]); // add router.isReady to the dependency array
+
 
   const [asideOpen, setAsideOpen] = useState(false);
 
   const AsideClickOpen = () => {
-    setAsideOpen(!asideOpen)
-  }
+    setAsideOpen(!asideOpen);
+  };
 
-
-
-  return <>
-    <ParentComponent appOpen={asideOpen} appAsideOpen={AsideClickOpen} />
-    <main>
-      <div className={asideOpen ? 'container' : 'container active'}>
-        <Component {...pageProps} />
-      </div>
-    </main>
-  </>
+  return (
+    <>
+      {loading ? (
+        // loading while load
+        <div className="flex flex-col flex-center wh_100">
+          <Loading />
+          <h1 className="mt-1">Loading...</h1>
+        </div>
+      ) : (
+        <>
+          <ParentComponent appOpen={asideOpen} appAsideOpen={AsideClickOpen} />
+          <main>
+            <div className={asideOpen ? "container" : "container active"}>
+              <Component {...pageProps} />
+            </div>
+          </main>
+        </>
+      )}
+    </>
+  );
 }
