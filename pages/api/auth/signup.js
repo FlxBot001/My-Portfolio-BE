008c57429bc0a992f481bf18e76b180f2f6e0f90
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-21 18:51:26 +0300

    Updated signup logic for login in, else creating newUser

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,27 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Profile } from "@/models/Profile";
+
+
+
+export default async function handler(req, res) {
+    await mongooseConnect();
+
+    const { email, password } = req.body;
+
+    try {
+        // check if the user already exists
+        const existingUser = await Profile.findOne({email});
+
+        if (existingUser) {
+            return res.status(400).json({message: 'User already exists'});
+        }
+
+        // create the user
+        const user = await Profile.create({email, password});
+
+        res.status(200).json({message: 'User created successfully', user: newUser});
+
+    } catch (error) {
+        res.status(500).json({error: 'Internal server error'});
+    }
+}
