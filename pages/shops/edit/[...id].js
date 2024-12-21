31289a6d9a9ffee53a7021d02315b95506a672b0
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-21 12:07:43 +0300

    Updated shop edit page

--- pages/shops/edit/[...id].js
+++ pages/shops/edit/[...id].js
@@ -1,9 +1,50 @@
+import Shop from "@/components/Shop";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { FcSurvey } from "react-icons/fc";
+import Head from "next/head";
+
 
 
 export default function Editshops() {
 
+    const router = useRouter();
+    const { id } = router.query;
+    const [productInfo, setProductInfo] = useState(null);
+
+    useEffect(() => {
+        if (!id) {
+            return;
+        } else {
+            axios.get('/api/shops?id=' + id).then(response => {
+                setProductInfo(response.data)
+            })
+        }
+    }, [id])
+
+
 
     return <>
-       
+        <Head>
+            <title>Update Product</title>
+        </Head>
+
+        <div className="blogpage">
+            <div className="titledashboard flex flex-sb">
+                <div>
+                    <h2>Edit <span>{productInfo?.title}</span></h2>
+                    <h1>ADMIN PANEL</h1>
+                </div>
+                <div className="breadcrumb">
+                    <FcSurvey /> <span>/</span> <span>Edit Product</span>
+                </div>
+            </div>
+            <div className="mt-3">
+                {productInfo && (
+                    <Shop {...productInfo} />
+                )}
+            </div>
+        </div>
     </>
 }
