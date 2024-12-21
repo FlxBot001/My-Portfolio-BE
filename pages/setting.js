dac5bd0c75aeb06ae1ea1910a39ae2e195e9f2ca
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-21 15:39:13 +0300

    Updated settings page with titles, profile image and titles

--- pages/setting.js
+++ pages/setting.js
@@ -1,3 +1,4 @@
+import { FcSettings } from "react-icons/fc";
 
 
 export default function Setting() {
@@ -7,7 +8,33 @@ export default function Setting() {
 
     return (
        <>
-       
+            <div className="settingpage">
+                <div className="titledashboard flex flex-sb">
+                    <div>
+                        <h2>Admin <span>Settings</span></h2>
+                        <h3>ADMIN PANEL</h3>
+                    </div>
+                    <div className="breadcrumb">
+                        <FcSettings /> <span>/</span> <span>Settings</span>
+                    </div>
+                </div>
+
+                <div className="profilesettings">
+                    <div className="leftprofile_details flex">
+                        <img src="/img/felix.png" alt="felix" />
+                        <div className="w-100">
+                            <div className="flex flex-sb flex-left mt-2">
+                                <h2>My Profile:</h2>
+                                <h3>FlxNjgn <br />DevOps Engineer</h3>
+                            </div>
+                            <div className="flex flex-sb mt-2">
+                                <h3>Phone:</h3>
+                                <input type="text" defaultValue={'+254-1112 55301'} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
        </>
     )
 
