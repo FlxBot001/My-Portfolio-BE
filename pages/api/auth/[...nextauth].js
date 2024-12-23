5ae37684c116f2588961afec2ee32dc44a20e5bd
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-23 15:33:02 +0300

    Updated nect auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,26 @@
 import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
+import CredentialsProvider from 'next-auth/providers/credentials';
+import connectToDatabase from '@/lib/mongodb';
 
 export default NextAuth({
   providers: [
+    CredentialsProvider({
+      //the name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      //The credentials option is required when using the Credentials Provider
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: {  label: "Password", type: "password" }
+      },
 
+      authorize: async (credentials) => {
+        // Add logic here to look up the user from the credentials supplied
+        const { db } = await connectToDatabase();
+        const collection = db.collection('admin');
+        
+      }
+    })
+    
     
   ]
 })
