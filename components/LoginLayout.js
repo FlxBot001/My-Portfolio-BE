c224eade08f742770c526e624f361d3ab627cfd4
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-23 11:01:47 +0300

    Updated LoginLayout component to render session from next auth

--- components/LoginLayout.js
+++ components/LoginLayout.js
@@ -8,18 +8,22 @@ export default function LoginLayout({ children }) {
     const { data: session, status } = useSession();
 
     if (status === 'loading') {
+        // Loading animation 
         return <div className="full-h flex flex-center">
-            <div className="loading-bar">Loading</div>
+            <div className="loading-bar">Loading...</div>
         </div>
     }
-
+    // router object
     const router = useRouter();
 
+    // If no session, redirect to login page
     if (!session) {
         router.push('/auth/signin');
         return null;
     }
 
+    
+    // If session, show children components 
     if (session) {
 
         return <>
