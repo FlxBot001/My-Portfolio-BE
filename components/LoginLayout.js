006bdb97328a37788b24f5193c7ba1f21d778671
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-23 09:05:24 +0300

    Updated auth logic that allows for account creation, login and authentication

--- components/LoginLayout.js
+++ components/LoginLayout.js
@@ -7,8 +7,10 @@ export default function LoginLayout({ children }) {
     const router = useRouter();
 
     useEffect(() => {
-        if (status === 'authenticated') {
+        if (status === 'authenticated' && router.pathname !== '/') {
             router.push('/'); // Redirect to home page after successful login
+        } else if (status === 'unauthenticated' && router.pathname !== '/auth/signup' && router.pathname !== '/auth/signin') {
+            router.push('/auth/signup'); // Redirect to signup page if user is not authenticated
         }
     }, [status, router]);
 
@@ -18,15 +20,5 @@ export default function LoginLayout({ children }) {
         </div>
     }
 
-    if (!session) {
-        router.push('/auth/signin');
-        return null;
-    }
-
-    if (session) {
-        
-        return <>
-            {children}
-        </>;
-    }
+    return session ? <>{children}</> : null;
 }
